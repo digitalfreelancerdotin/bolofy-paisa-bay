9470cc37a27b32f781b8cd4ef53c33653520da5f
deepanshu921372 <sharmadeepanshu1005@gmail.com>
2025-03-17 17:42:39 +0530

    put credentials to env

--- src/components/MyPoliciesPage.js
+++ src/components/MyPoliciesPage.js
@@ -8,14 +8,15 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
+
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBcofTcJPW7AKlM787-dSxaq2g_mPqfv1M",
-  authDomain: "study-d0a4b.firebaseapp.com", 
-   projectId: "study-d0a4b",
-  storageBucket: "study-d0a4b.appspot.com", 
-   messagingSenderId: "193271703266",
-  appId: "1:193271703266:web:df0073da99779d27c8e0bb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN, 
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET, 
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
